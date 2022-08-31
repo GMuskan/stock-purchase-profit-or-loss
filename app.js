42e0dd38d677c938784e08368dc6d360b363d11e
GMuskan <55504711+GMuskan@users.noreply.github.com>
2022-08-31 16:03:12 +0530

    fix: percentage calculation fixed

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ function submitHandler(){
 function calculateProfitAndLoss(initial, quantity, current){
     if(initial>current){
         var loss = ((initial - current) * quantity).toFixed(2);
-        var lossPercent = ((loss/initial) * 100).toFixed(2);
+        var lossPercent = ((loss/(initial*quantity)) * 100).toFixed(2);
         showOutput(`Hey, the loss is ${loss} and the loss percentage is ${lossPercent}%`);
         outputBox.style.color='red';
        // outputBox.innerText = `Hey, the loss is ${loss} and the loss percentage is ${lossPercent}%`;
     }else if(current>initial){
         var profit = ((current - initial) * quantity).toFixed(2);
-        var profitPercent = ((profit/initial) * 100).toFixed(2);
+        var profitPercent = ((profit/(initial*quantity)) * 100).toFixed(2);
         showOutput(`Hey, the profit is ${profit} and the profit percentage is ${profitPercent}%`);
         outputBox.style.color='green';
         //outputBox.innerText = `Hey, the profit is ${profit} and the profit percentage is ${profitPercent}%`;
